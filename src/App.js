3dd1c8724936026f4c4263602a57f5c616e29b73
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-10-27 13:46:37 +0100

    Added a server script to run the questions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  });
   return (
     <div className="app">
       <Header />
@@ -11,5 +37,5 @@ export default function App() {
         <p>Question</p>
       </Main>
     </div>
-  ); 
+  );
 }
