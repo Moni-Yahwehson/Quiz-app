53efe00ce4a7c9a9e1721b5600c0fdf2bf1e2e0f
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-10-30 11:02:32 +0100

    Added an onClick function to start screen button

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
 
+    case "start":
+      return { ...state, status: "active" };
+
     default:
       throw new Error("Action unknown");
   }
@@ -41,7 +44,9 @@ export default function App() {
       <Main className="main">
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
         {status === "active" && <Questions />}
       </Main>
     </div>
